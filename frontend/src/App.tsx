46f8bf5a5953f69b56e6826cf8daf1100974c39d
ThaynaDias <thaynadiasnascimento@gmail.com>
2022-07-11 22:51:56 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import NotificationButton from "./components/NotificationButton"
-
+import SalesCard from "./components/SalesCard";
+import Header from "./components/Header";
 
 function App() {
-    return(
-      <>
-      <h1>Olá mundo</h1>
-      <NotificationButton/>
-      </>
-    )
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          <SalesCard/>
+            
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
